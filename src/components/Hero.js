512934dbcc5424facb455f7dd0462c2e5024a3e6
unknown <noepacio1625@gmail.com>
2023-09-13 12:06:12 -0300

    first

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,10 @@
 const Hero = () => {
   return (
     <div
-      className={`hero flex justify-center items-center w-full h-[600px] bg-[url('/assets/12monitos.jpg')] bg-cover bg-top mb-12`}
+      className={`hero flex justify-center items-center w-full h-[600px] bg-[url('/assets/background.jpeg')] bg-cover bg-top mb-12`}
     >
       <div className=' bg-[rgba(0,0,0,.6)] w-full h-full flex justify-center items-center'>
-        <h1 className=' text-7xl'>Welcome to the machine</h1>
+        <h1 className=' text-7xl'>Welcome to NoeFlix</h1>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,12 +6,12 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center h-14 bg-slate-800 px-5'>
       <div className=' w-32 h-auto sm:w-36 md:w-[300px]'>
-        <Image src={`/assets/umai.svg`} width={300} height={64} alt={`Logo`} />
+        <Image src={`/assets/logo.png`} width={50} height={64} alt={`Logo`} />
       </div>
       <nav className='hidden sm:block'>
         <ul className='flex justify-end items-center gap-4'>
-          <li>About</li>
-          <li>Contact</li>
+          <li>Películas</li>
+          <li>Series</li>
         </ul>
       </nav>
       <div className='sm:hidden'>
