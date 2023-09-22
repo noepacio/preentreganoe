ecb49480e0b4cdfb3964ad62fcb6e9ba57507831
unknown <noepacio1625@gmail.com>
2023-09-21 22:03:01 -0300

    holiss

--- src/components/ShowCard.js
+++ src/components/ShowCard.js
@@ -5,7 +5,7 @@ const ShowCard = ({ actualShow }) => {
   const { show } = actualShow;
   const { name, id, image } = show;
   return (
-    <div className='show_container col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 flex flex-col gap-3 justify-start items-center'>
+    <div className=' show_container col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 flex flex-col gap-3 justify-start items-center'>
       <div className='border border-gray-900 border-solid mb-2 w-full h-auto'>
         <Image src={image?.original} width={680} height={1000} alt={name} />
       </div>
